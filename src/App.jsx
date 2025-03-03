052bf7e1d6d543489573d41ab4dc32a3001c83e8
tsigabuwalker <Seyoumwalker@gmail.com>
2025-03-03 20:25:38 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Header from "./Header.jsx"
+import Footer from "./Footer.jsx"
+import Food from "./Food.jsx";
+
+function App() {
+  return(
+    <div>
+<Header />
+<Food />
+<Food />
+<Food />
+<Footer />
+    </div>
+    
+
+  );
+  
+  
+}
+
+export default App
---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+    return(
+        <footer>
+            <p> &copy ; { new Date().getFullYear()} All rights reserved.</p>
+            <p><a href="#">Terms of Service</a> | <a href="#">Privacy Policy</a></p>
+        </footer>
+    );
+
+
+}
+export default Footer;
