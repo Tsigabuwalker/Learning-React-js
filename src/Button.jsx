d462714ea009787b39ead8cb3aea35ac7555ff53
tsigabuwalker <Seyoumwalker@gmail.com>
2025-03-10 11:39:48 +0300

    Click event

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,8 +1,24 @@
-function Button(){
-    return(
-        <button className="button">Click Me</button>
+import React from 'react';
 
-    );
+function Button() {
+let count = 0;
 
+const btn2 = (name) => {
+    if(count < 3){
+        count++;
+        console.log(` ${name} Button Clicked ${count} times. Hello`);
+    }
+    else{
+        console.log("Button has reached its maximum limit. Clicking is disabled.");
+    }
 }
+    
+
+    return (
+        <button onClick={() => btn2("Tsigabu")} className="button">
+            Click Me 😄
+        </button>
+    );
+}
+
 export default Button;
